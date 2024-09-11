3dd51755f6cef3bc65f24aa13a5662095cdb047f
Данила Григорьев <danila.sar@yandex.ru>
2024-09-11 16:39:26 +0400

    Адаптация базы данных под GitHub

--- database/dump.sql
+++ database/dump.sql
@@ -16,14 +16,18 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+ALTER TABLE ONLY public.sessions DROP CONSTRAINT session_users_id_fk;
 DROP INDEX public.users_login_uindex;
 DROP INDEX public.posts_title_index;
+DROP INDEX public.posts_md_file_index;
 ALTER TABLE ONLY public.users DROP CONSTRAINT users_pk;
+ALTER TABLE ONLY public.sessions DROP CONSTRAINT session_pk;
 ALTER TABLE ONLY public.posts DROP CONSTRAINT posts_pk;
 ALTER TABLE public.users ALTER COLUMN id DROP DEFAULT;
 ALTER TABLE public.posts ALTER COLUMN id DROP DEFAULT;
 DROP SEQUENCE public.users_id_seq;
 DROP TABLE public.users;
+DROP TABLE public.sessions;
 DROP SEQUENCE public.posts_id_seq;
 DROP TABLE public.posts;
 SET default_tablespace = '';
@@ -37,7 +41,8 @@ SET default_table_access_method = heap;
 CREATE TABLE public.posts (
     id integer NOT NULL,
     title character varying(512) NOT NULL,
-    content text NOT NULL
+    description character varying(1024) NOT NULL,
+    md_file character varying(512) DEFAULT ''::text NOT NULL
 );
 
 
@@ -64,6 +69,19 @@ ALTER SEQUENCE public.posts_id_seq OWNER TO postgres;
 
 ALTER SEQUENCE public.posts_id_seq OWNED BY public.posts.id;
 
+
+--
+-- Name: sessions; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.sessions (
+    token character varying(512) NOT NULL,
+    user_id integer NOT NULL,
+    expires timestamp without time zone DEFAULT (CURRENT_TIMESTAMP + '69 days'::interval day) NOT NULL
+);
+
+
+ALTER TABLE public.sessions OWNER TO postgres;
 
 --
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
@@ -114,20 +132,12 @@ ALTER TABLE ONLY public.posts ALTER COLUMN id SET DEFAULT nextval('public.posts_
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
 
-
---
--- Data for Name: posts; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.posts (id, title, content) FROM stdin;
-\.
-
-
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.users (id, login, name, password_hash) FROM stdin;
+1	danilasar	Даня	bbc9c27f06c5275429f523e1a3955b9e33f1a90d88c19ef260da9e500494d6cf1f591d05afaedd64778329c63b023b12e5706e84de4972cfa82fbbc3fa66761d
 \.
 
 
@@ -135,14 +145,14 @@ COPY public.users (id, login, name, password_hash) FROM stdin;
 -- Name: posts_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.posts_id_seq', 1, false);
+SELECT pg_catalog.setval('public.posts_id_seq', 3, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 1, false);
+SELECT pg_catalog.setval('public.users_id_seq', 1, true);
 
 
 --
@@ -154,11 +164,26 @@ ALTER TABLE ONLY public.posts
 
 
 --
+-- Name: sessions session_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT session_pk PRIMARY KEY (token);
+
+
+--
 -- Name: users users_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pk PRIMARY KEY (id);
+
+
+--
+-- Name: posts_md_file_index; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX posts_md_file_index ON public.posts USING btree (md_file);
 
 
 --
@@ -173,6 +198,14 @@ CREATE INDEX posts_title_index ON public.posts USING btree (title);
 --
 
 CREATE UNIQUE INDEX users_login_uindex ON public.users USING btree (login);
+
+
+--
+-- Name: sessions session_users_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT session_users_id_fk FOREIGN KEY (user_id) REFERENCES public.users(id);
 
 
 --
